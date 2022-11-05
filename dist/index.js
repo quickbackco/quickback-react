b7d828de1d47502e9e093dfd4e6a53cc2705a8cb
marcm <morandi.marc@hotmail.fr>
2022-11-05 12:31:23 +0100

    second commit

--- dist/index.js
+++ dist/index.js
@@ -9,12 +9,12 @@ var _client = _interopRequireDefault(require("react-dom/client"));
 require("./index.css");
 var _quickback = _interopRequireDefault(require("./quickback"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-// import Quickback from 'quickback';
+// import Quickback from '@quickbackco/quickback-react'
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //     <div>
 //       <h1>Hello</h1>
-//       <Quickback url="https://quickback-api-stg.onrender.com/f/tw4rqh6xrzzt" />
+//       {/* <Quick url="" /> */}
 //     </div>
 // );
 var _default = _quickback.default;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-// import Quickback from 'quickback';
 import Quickback from './quickback';
+// import Quickback from '@quickbackco/quickback-react'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //     <div>
 //       <h1>Hello</h1>
-//       <Quickback url="https://quickback-api-stg.onrender.com/f/tw4rqh6xrzzt" />
+//       {/* <Quick url="" /> */}
 //     </div>
 // );
 
