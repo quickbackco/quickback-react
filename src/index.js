a5f8811041cf0cf12cbdcd7e51555a5b1a768c18
marcm <morandi.marc@hotmail.fr>
2022-11-09 12:31:14 +0100

    fix css parameters & env

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Quickback from './quickback'
-import styled from 'styled-components'
-// import './quickback.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <div>
       <h1>Hello</h1>test
-      <Quickback placeholder="Customize ?" style={{focus: {width: '24rem', height: '10rem'}}} />
+      <Quickback id={process.env.REACT_APP_TEST_ID} placeholder="Customize ?"  />
     </div>
 );
